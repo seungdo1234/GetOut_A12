17f30cc8bba693dc00a1376743bde8047340e5fa
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-05-16 19:32:52 +0900

    Power Up

--- Assets/Scripts/GameData/PlayerData.cs
+++ Assets/Scripts/GameData/PlayerData.cs
@@ -4,7 +4,13 @@ public class PlayerData : CharacterData
 {
     [Header("# Player Stat")]
     [SerializeField] private string playerName;
+    [SerializeField] private int power;
     [SerializeField] private int bulletNum;
     [SerializeField] private float bulletAngle;
-    
+
+
+    public void PowerUp()
+    {
+        power++;
+    }
 }
