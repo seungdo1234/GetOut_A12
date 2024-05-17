9e2515e2bc49b3c5b9c39b27c53c753cb03703a3
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-05-17 21:50:59 +0900

    배경 수정 중

--- Assets/Scripts/GamePlay/Behaviors/PoolObject.cs
+++ Assets/Scripts/GamePlay/Behaviors/PoolObject.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public abstract class PoolObject : MonoBehaviour
 {
-    public abstract void BulletInit(float damage ,float bulletSpeed , LayerMask targetLayer);
+    public abstract void BulletInit(float damage, float bulletSpeed, LayerMask targetLayer);
     
+
 }
