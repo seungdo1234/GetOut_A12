107721141d120515203730acc5dc50f7bba4c763
Shawoo3719 <shawoo3719@naver.com>
2024-05-19 19:04:18 +0900

    [Chore] 구현해야할 목록 주석달아놓음

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+
+    // 메인메뉴씬 구성 후, 메인씬과 연결 하기.
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
+
+
+    // Start버튼 구현 중
     public void OnCllickNewGame()
     {
+        //버튼으로 두어야 하는지 확인 필요하므로, 4주차 프로젝트 해설 참고하여 구현하기.
         Debug.Log("새 게임");
     }
 
+
+    // Quit버튼 구현 완료. Quit 버튼 누르면 게임종료.
     public void OnclickQuit()
     {
 #if UNITY_EDITOR
@@ -30,8 +43,12 @@ public class NewBehaviourScript : MonoBehaviour
 #endif
     }
 
+    // PlayerName 버튼 누르면 2~10글자의 이름 입력을 저장하기. 이후 어떻게 호출할 수 있는지 연구
     public void OnclickPlayerName()
     {
         Debug.Log("플레이어 이름 입력");
     }
+
 }
+
+
