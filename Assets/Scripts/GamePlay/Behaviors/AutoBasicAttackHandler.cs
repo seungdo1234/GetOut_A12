5bc80d74c3623beb77f60a52e8f3420948a08adc
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-05-21 23:12:11 +0900

    1

--- Assets/Scripts/GamePlay/Behaviors/AutoBasicAttackHandler.cs
+++ Assets/Scripts/GamePlay/Behaviors/AutoBasicAttackHandler.cs
@@ -3,46 +3,31 @@ using UnityEngine;
 
 public class AutoBasicAttackHandler : TopDownShooting
 {
-    private Coroutine scoutAttackCoroutine;
-    private FlightStatHandler flightStat;
-
-    private bool isBasicAttackLock;
-    protected void Awake()
-    {
-        flightStat = GetComponent<FlightStatHandler>();
-    }
+    private Coroutine attackCoroutine;
 
     private void Start()
     {
-        PlayAutoScoutAttack();
+        PlayAutoBasicAttack();
     }
 
-    public void PlayAutoScoutAttack()
+    public void PlayAutoBasicAttack()
     {
-        // 현재 비행기가 살아있는 경우에만 총기 발사
-        if (scoutAttackCoroutine != null)
+        // 이미 코루틴이 실행중이라면 중지하고 다시 시작 (겹치는 것 방지)
+        if (attackCoroutine != null)
         {
-            StopCoroutine(scoutAttackCoroutine);
+            StopCoroutine(attackCoroutine);
         }
 
-        scoutAttackCoroutine = StartCoroutine(AutoScoutAttackCoroutine());
+        attackCoroutine = StartCoroutine(AutoBasicAttackCoroutine());
     }
-    private IEnumerator AutoScoutAttackCoroutine()
+    private IEnumerator AutoBasicAttackCoroutine()
     {
         WaitForSeconds wait = new WaitForSeconds(flightStat.CurrentStat.AtkDelay);
 
         while (flightStat.CurrentStat.EFlightStatus == EFlightStatus.Alive)
         {
-            if (!isBasicAttackLock)
-            {
-                Shooting(flightStat.CurrentStat);
-            }
+            Shooting(flightStat.CurrentStat);
             yield return wait;
         }
-    }
-
-    public void BasicAttackLock(bool isTrue) // 기본 공격 Lock
-    {
-        isBasicAttackLock = isTrue;
     }
 }
--- Assets/Scripts/GamePlay/Behaviors/DisableOnDeath.cs
+++ Assets/Scripts/GamePlay/Behaviors/DisableOnDeath.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class DisableOnDeath : MonoBehaviour
 {
-    public void OnDisableScout()
+    public void OnDisableFlight()
     {
         gameObject.SetActive(false);
-        ItemManager.Instance.DropInfo(this.gameObject);
     }
+
 }
