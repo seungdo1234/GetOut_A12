ff075384978bf5c055bbba7965011d2bbac407ec
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-05-21 23:51:51 +0900

    Create AutoMultipleAttackHandler.cs

---
+++ Assets/Scripts/GamePlay/Behaviors/AttackHandler/AutoMultipleAttackHandler.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMultipleAttackHandler : TopDownShooting
+{
+    [SerializeField] private List<Transform> firePositions;
+    private Coroutine attackCoroutine;
+
+    private void Start()
+    {
+        PlayAutoMultipleAttack();
+    }
+
+    private void PlayAutoMultipleAttack()
+    {
+        // 이미 코루틴이 실행중이라면 중지하고 다시 시작 (겹치는 것 방지)
+        if(attackCoroutine != null)
+        {
+            StopCoroutine(attackCoroutine);
+        }
+
+        attackCoroutine = StartCoroutine(AutoMultipleAttackCoroutine());
+    }
+
+    private IEnumerator AutoMultipleAttackCoroutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(flightStat.CurrentStat.AtkDelay);
+        while (flightStat.CurrentStat.EFlightStatus == EFlightStatus.Alive)
+        {
+            // firePoints들을 순회하면서 발사
+            foreach(Transform firePosition in firePositions)
+            {
+                Shooting(flightStat.CurrentStat, firePosition);
+                yield return wait;
+            }
+        }
+    }
+}
